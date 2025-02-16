27d8845bb3ec304085c89981fd234383d8e2bb41
Mrinalani <pandeymrinalani02@gmail.com>
2025-02-16 22:17:27 +0530

    add integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import abi from "../../artifacts/contracts/Chai.sol/chai.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+  const connectWallet = async () => {
+    const contractAddress = "0xa2b05Ece5168aF2cDE09Ad3970d11Ce94321673e";
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const account = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const provider = new ethers.BrowserProvider(ethereum);
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi.abi, signer);
+        setState({provider, signer, contract});
+        console.log(state);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    connectWallet()
+  },[])
+
+  return <></>;
 }
 
-export default App
+export default App;
